c86448db24035712f4d7bbdf2e24551715113b15
petliukmaksym <maksym.petliuk@gmail.com>
2025-06-29 18:17:12 +0300

    Fixed PaymentController

--- src/main/java/com/example/bookingapp/controller/PaymentController.java
+++ src/main/java/com/example/bookingapp/controller/PaymentController.java
@@ -21,8 +21,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/payments")
 @RequiredArgsConstructor
-@Tag(name = "Payment Management", description = "\n" +
-        "API for payment management")
+@Tag(name = "Payment Management", description = "\n"
+        + "API for payment management")
 public class PaymentController {
 
     private final PaymentService paymentService;
